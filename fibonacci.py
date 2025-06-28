23b00e1516ba35b4a1c9fdced2d143a9f33e00ce
Tingyang Li <tingyang.li@outlook.com>
2025-06-28 06:52:50 +0300

    Add Fibonacci program

---
+++ fibonacci.py
@@ -0,0 +1,27 @@
+print('moikka')
+
+n = 1798
+
+#print(baobaofibs[i - 1])
+#print(baobaofibs[i - 2])
+#print(len(baobaofibs))
+
+
+def fibonacci (n):
+    baobaofibs = [0, 1]
+    a = 0            
+    while a < n:
+        i = len(baobaofibs)
+        #x= (x-1)+(x-2)
+        new = baobaofibs[i - 1] + baobaofibs[i - 2]
+        baobaofibs.append(new)
+       
+        a += 1
+    i = len(baobaofibs)
+    
+    print (baobaofibs[i-1])
+
+
+fibonacci(n)
+
+            
